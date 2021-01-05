8f841eec5e6ee059f0e4a52cc5a96d0cd61d9344
ahlvin <42684180+ahlvin@users.noreply.github.com>
2021-01-04 22:19:35 -0500

    Create RecipeBootstrap.java

---
+++ spring5-recipe-app/src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -0,0 +1,15 @@
+package guru.springframework.bootstrap;
+
+import guru.springframework.domain.Recipe;
+import org.springframework.boot.CommandLineRunner;
+
+/**
+ * Created by vineet  on 1/4/2021.
+ */
+public class RecipeBootstrap extends CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        Recipe recipe = new Recipe();
+
+    }
+}
