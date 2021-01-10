badd68ba1ca7f5f2d333b222dcee8ac9c5ff64e1
ahlvin <42684180+ahlvin@users.noreply.github.com>
2021-01-10 00:24:15 -0500

    142- Added Bootstrap data

--- spring5-recipe-app/src/main/java/guru/springframework/repositories/RecipeRepository.java
+++ spring5-recipe-app/src/main/java/guru/springframework/repositories/RecipeRepository.java
@@ -1,11 +1,15 @@
 package guru.springframework.repositories;
 
+import guru.springframework.domain.Category;
 import guru.springframework.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
 
 /**
  * Created by vineet  on 1/3/2021.
  */
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
-
+//    @Override
+//    <S extends Recipe> Iterable<S> saveAll(Iterable<S> iterable);
 }
